4fd33dbc0d0d090a2f9ede1a87cf65a65b9c39ca
Andrey <aprishlov@gmail.com>
2024-10-28 20:05:56 +0300

    snake_case to camelCase

--- src/base/GameMap.java
+++ src/base/GameMap.java
@@ -6,18 +6,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class GameMap {
-    Map<Coordinates, Entity> entity_list;
+    Map<Coordinates, Entity> entityList;
     private final int length;
     private final int width;
 
     GameMap(){
-        entity_list = new HashMap<>();
+        entityList = new HashMap<>();
         this.length = 10;
         this.width = 10;
     }
 
     GameMap(int length, int width){
-        entity_list = new HashMap<>();
+        entityList = new HashMap<>();
         this.length = length;
         this.width = width;
     }
@@ -31,23 +31,23 @@ public class GameMap {
     }
 
     public boolean isFree(Coordinates coordinates){
-        return !entity_list.containsKey(coordinates);
+        return !entityList.containsKey(coordinates);
     }
 
     public Entity getEntity(Coordinates coordinates){
-        return entity_list.get(coordinates);
+        return entityList.get(coordinates);
     }
 
     public Map<Coordinates, Entity> getEntitiesList(){
-        return entity_list;
+        return entityList;
     }
 
     public void putEntity(Coordinates coordinates, Entity entity){
-        entity_list.put(coordinates, entity);
+        entityList.put(coordinates, entity);
     }
 
     public void removeEntity(Coordinates coordinates){
-        entity_list.remove(coordinates);
+        entityList.remove(coordinates);
     }
 
     public void moveEntity(Coordinates from, Coordinates to){
