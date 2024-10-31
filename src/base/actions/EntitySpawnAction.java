15ee0eec4f198aac06e2c6803e17e3ba515d88e5
Andrey <aprishlov@gmail.com>
2024-10-31 16:13:09 +0300

    utils->Utils

--- src/base/actions/EntitySpawnAction.java
+++ src/base/actions/EntitySpawnAction.java
@@ -2,12 +2,12 @@ package base.actions;
 
 import base.Coordinates;
 import base.GameMap;
+import base.Utils;
 import base.entities.creatures.Herbivore;
 import base.entities.creatures.Predator;
 import base.entities.objects.Grass;
 import base.entities.objects.Rock;
 import base.entities.objects.Tree;
-import base.utils;
 
 public class EntitySpawnAction {
     public static void spawnAllEntities(GameMap map){
@@ -19,35 +19,35 @@ public class EntitySpawnAction {
     }
 
     public static void spawnPredators(GameMap map){
-        for (int i = 0; i < utils.maxPredatorCount; i++) {
+        for (int i = 0; i < Utils.maxPredatorCount; i++) {
             Coordinates coordinates = Coordinates.generateFreeCoordinates(map);
             map.putEntity(coordinates, new Predator());
         }
     }
 
     public static void spawnHerbivores(GameMap map){
-        for (int i = 0; i < utils.maxHerbivoreCount; i++) {
+        for (int i = 0; i < Utils.maxHerbivoreCount; i++) {
             Coordinates coordinates = Coordinates.generateFreeCoordinates(map);
             map.putEntity(coordinates, new Herbivore());
         }
     }
 
     public static void spawnGrass(GameMap map){
-        for (int i = 0; i < utils.maxGrassCount; i++) {
+        for (int i = 0; i < Utils.maxGrassCount; i++) {
             Coordinates coordinates = Coordinates.generateFreeCoordinates(map);
             map.putEntity(coordinates, new Grass());
         }
     }
 
     public static void spawnRocks(GameMap map){
-        for (int i = 0; i < utils.maxRockCount; i++) {
+        for (int i = 0; i < Utils.maxRockCount; i++) {
             Coordinates coordinates = Coordinates.generateFreeCoordinates(map);
             map.putEntity(coordinates, new Rock());
         }
     }
 
     public static void spawnTrees(GameMap map){
-        for (int i = 0; i < utils.maxTreeCount; i++) {
+        for (int i = 0; i < Utils.maxTreeCount; i++) {
             Coordinates coordinates = Coordinates.generateFreeCoordinates(map);
             map.putEntity(coordinates, new Tree());
         }
